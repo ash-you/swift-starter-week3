2c41486e13ffbc5a7afe145875b18b085a704b74
유연수 <yeon@yuyeonsuui-MacBookPro.local>
2022-05-12 00:24:52 +0900

    ADD: STEP1 큰틀의 코드만 작성

--- CodeStarterCamp_Week3/main.swift
+++ CodeStarterCamp_Week3/main.swift
@@ -8,5 +8,46 @@
 
 import Foundation
 
-print("Hello, World!")
+struct Person {
+    var name: String = ""
+    var ownMoney: Double = 0
+    var gender: Gender!
+    
+    enum Gender {
+        case male, female
+    }
+    
+    mutating func buyCoffee(priceOfCoffee: Double) {
+        self.ownMoney = self.ownMoney - priceOfCoffee
+        
+        print("""
+              \(self.name)이(가) \(priceOfCoffee)원의 커피를 구매하였습니다.\t
+              남은 잔액은 \(self.ownMoney) - \(priceOfCoffee)원 입니다.
+              """)
+    }
+    
+    init(name: String, ownMoney: Double, gender: Gender) {
+        self.name = name
+        self.ownMoney = ownMoney
+        self.gender = gender
+    }
+}
 
+struct Coffeeshop {
+    var sales: Double = 0
+    var barista: Person?
+    
+    func takeAOrder(orderer: Person) -> String {
+        return "\(orderer) 고객으로부터 주문을 받습니다."
+    }
+}
+
+enum Coffee {
+    case americano, latte
+}
+
+let misterLee: Person = Person(name: "lee", ownMoney: 0, gender: Person.Gender.male)
+let missKim: Person = Person(name: "kim", ownMoney: 0, gender: Person.Gender.female)
+
+var yagombucks: Coffeeshop = Coffeeshop()
+yagombucks.barista = misterLee
